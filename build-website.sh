a7349b253cb440237ca90a6088ab25b2c82cdab6
Ryan Curtin <ryan@ratml.org>
2018-11-15 21:06:41 -0500

    More robust website building script.

--- build-website.sh
+++ build-website.sh
@@ -6,14 +6,24 @@
 # Before using Jekyll, this first unpacks the most recent documentation from the
 # most recent release of ensmallen that is available in the files/ directory.
 
+if [ "$#" -ne 1 ]; then
+  echo "Argument required: directory to build website into!";
+  exit 1;
+fi
+
 ensmallen_newest=`ls files/ensmallen-*.tar.gz | sort | tail -1`;
 ensmallen_dir=`basename "$ensmallen_newest" .tar.gz`;
-tar -xf "$ensmallen_newest" $ensmallen_dir/doc/optimizers.md
-tar -xf "$ensmallen_newest" $ensmallen_dir/doc/function_types.md
-tar -xf "$ensmallen_newest" $ensmallen_dir/HISTORY.md
+tar -xf "$ensmallen_newest" $ensmallen_dir/doc/optimizers.md;
+tar -xf "$ensmallen_newest" $ensmallen_dir/doc/function_types.md;
+tar -xf "$ensmallen_newest" $ensmallen_dir/HISTORY.md;
 
-mv $ensmallen_dir/doc/optimizers.md optimizers.md
-mv $ensmallen_dir/doc/function_types.md function_types.md
-mv $ensmallen_dir/HISTORY.md history.md
+mv $ensmallen_dir/doc/optimizers.md optimizers.md;
+mv $ensmallen_dir/doc/function_types.md function_types.md;
+mv $ensmallen_dir/HISTORY.md history.md;
 
-jekyll b
+mkdir tmp_site;
+mkdir old_site;
+jekyll b -d tmp_site/ -b . \
+    && mv "$1/*" old_site/ \
+    && mv tmp_site/* "$1/" \
+    && rm -rf tmp_site old_site;
